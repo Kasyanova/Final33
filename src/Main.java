b1379e145706edebca3388fdbe79a1c232e544b8
KKasyanova <eliseeva.kseniya@gmail.com>
2022-10-07 14:30:42 +0500

    итоговая задача 3 длина строки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+/*
+Напишите программу, где пользователь вводит сначала количество строк n, затем сами строки. Среди данных строк найти строку с максимальным количеством различных символов. Если таких строк будет много, то вывести первую.
+Пример для теста работы программы:
+Количество строк: 3
+Строка 1: привет
+Строка 2: анализ
+Строка 3: 111111111111
+Ответ: привет
+* */
+
+package com.max.idea;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите количество строк: ");
+        int number = in.nextInt();
+        String[] myArray = new String[number];
+        in.nextInt();
+        for (int i=0;i<myArray.length;i++){
+            System.out.println("Введите строку номер "+i);
+            myArray[i] = in.nextLine();
+            System.out.println("строка = "+myArray[i]);
+        }
+
+        System.out.println("");
     }
 }
